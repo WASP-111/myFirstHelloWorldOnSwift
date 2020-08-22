19d6e0cae696460f03de777d2c07bb32857c3139
Anna Goncharova <bkljn4g@gmail.com>
2020-08-22 16:02:07 +0300

    add second view controller

--- privet/ViewController.swift
+++ privet/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
 
     @IBOutlet var helloWorldLabel: UILabel!
     @IBOutlet var showTextButton: UIButton!
+    @IBOutlet var transitionControllerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         helloWorldLabel.isHidden = true
         showTextButton.layer.cornerRadius = 10
+        transitionControllerButton.layer.cornerRadius = 10
     }
 
     @IBAction func showTextedButtonPressed() {
